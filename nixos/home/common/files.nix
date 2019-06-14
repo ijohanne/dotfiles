24ee22385ec7882e6bcbf356cdaf4206f7af5ef2
Ian Johannesen <ij@concordium.com>
2019-06-14 09:22:41 +0200

    wip sway

--- nixos/home/common/files.nix
+++ nixos/home/common/files.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let 
+  dots = "${config.home.homeDirectory}/.dotfiles";
+in
 {
   home.file = {
+    ".config/sway/config".source = "${dots}/sway/config";
   };
 }
--- nixos/home/common/packages.nix
+++ nixos/home/common/packages.nix
@@ -15,5 +15,7 @@
     xlockmore
     rofi
     xautolock
+    bemenu
+    termite
   ];
 }
