5ec434cc7cf4d03a7154f8972deb421cf3e047ac
Ian Johannesen <ij@ip-172-28-8-197.eu-west-1.compute.internal>
2019-07-25 09:51:40 +0000

    add box

---
+++ nixos/home/jumpbox.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.overlays = [ (import ./overlays/package-upgrades) ];
+
+  imports = [
+    ./common/fonts-themes.nix
+    ./common/packages.nix
+    ./common/programs.nix
+    ./common/services.nix
+    ./common/files.nix
+    ./common/environment.nix
+  ];
+}
