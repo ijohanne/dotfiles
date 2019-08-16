817e3d42c08f6aee87bf9ae86747d741fa3d56a5
Ian Johannesen <ij@concordium.com>
2019-08-16 15:23:05 +0200

    Enable OpenVPN service

--- nixos/machines/ij-laptop/configuration.nix
+++ nixos/machines/ij-laptop/configuration.nix
@@ -11,4 +11,10 @@
 
   networking.hostName = "ij-laptop";
   networking.hostId = "d4c95480";
+
+  services.openvpn.servers.office-vpn = {
+    config = "config /home/ij/.config/vpn/ij-mobile.ovpn";
+    autoStart = false;
+    updateResolvConf = true;
+  };
 }
