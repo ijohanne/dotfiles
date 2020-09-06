b26c0285789a7a978d2408a87e06a28dd2e76c92
Ian Johannesen <ij@opsplaza.com>
2020-09-07 00:51:47 +0200

    Optmize NUR import

--- nixos/home/common/programs.x11.nix
+++ nixos/home/common/programs.x11.nix
@@ -1,11 +1,8 @@
-let
-  sources = import ../nix;
-  pkgs = import sources.nixpkgs { overlays = [ sources.mozilla-overlay ]; };
-  nur = import sources.nur { inherit pkgs; };
+let sources = import ../nix;
 in {
   programs.firefox = {
     enable = true;
-    extensions = with nur.repos.rycee.firefox-addons; [
+    extensions = with sources.nur.repos.rycee.firefox-addons; [
       ublock-origin
       lastpass-password-manager
       reddit-enhancement-suite
--- nixos/home/nix/default.nix
+++ nixos/home/nix/default.nix
@@ -1,8 +1,10 @@
-let sources = import ./sources.nix;
+let
+  sources = import ./sources.nix;
+  pkgs = import sources.nixpkgs { };
 in rec {
   home-manager = sources.home-manager;
   nixpkgs = sources.nixpkgs;
-  nur = sources.NUR;
+  nur = import sources.NUR { inherit pkgs; };
   neovim-overlay = import sources.neovim-overlay;
   mozilla-overlay = import sources.mozilla-overlay;
 }
