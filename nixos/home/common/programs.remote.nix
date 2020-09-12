123a6d295f4148dec482bdf041e705b6a313de03
Ian Johannesen <ij@opsplaza.com>
2020-09-12 10:15:31 +0200

    Cleanup

--- nixos/home/common/programs.remote.nix
+++
@@ -1,8 +0,0 @@
-let
-  sources = import ../nix;
-  pkgs = import sources.nixpkgs { overlays = [ ]; };
-in {
-  programs.zsh = {
-    oh-my-zsh = { plugins = [ "git" "zsh_reload" "kubectl" ]; };
-  };
-}
