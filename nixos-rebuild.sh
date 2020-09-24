201accea5074ff4d8e6e4dadc6cd95a3a142d9a1
Ian Johannesen <ij@opsplaza.com>
2020-09-24 18:22:18 +0200

    Preserve env

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
-nix-shell --run "sudo nixos-rebuild -I nixos-config=/etc/nixos/configuration.nix $@"
+nix-shell --run "sudo -E nixos-rebuild -I nixos-config=/etc/nixos/configuration.nix $@"
