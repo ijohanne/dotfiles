3b727594de6c8f331513ca669662a6bb5647f8a6
Ian Johannesen <ij@opsplaza.com>
2020-09-24 18:13:51 +0200

    Fixes

--- configs/fish/init.fish
+++ configs/fish/init.fish
@@ -10,11 +10,6 @@ zoxide init fish | source
 # Enable direnv
 eval (direnv export fish)
 
-        case "switch"
-                $HOME/.dotfiles/switch.sh
-        case '*'
-                echo "Only switch is supported"
-end
 alias home-manager="$HOME/.dotfiles/home-manager.sh"                        
 alias nixos-rebuild="$HOME/.dotfiles/nixos-rebuild.sh"
 alias nixfmt-recursive="find . -name \*nix -type f -exec nixfmt {} \;"
