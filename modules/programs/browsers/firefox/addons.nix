34f26c589fdd6d5f31235eedc87c79242877c00a
Ian Johannesen <ij@opsplaza.com>
2020-11-01 03:03:37 +0100

    Add firefox extension

---
+++ modules/programs/browsers/firefox/addons.nix
@@ -0,0 +1,17 @@
+{ buildFirefoxXpiAddon, stdenv }: {
+  "darkreader" = buildFirefoxXpiAddon {
+    pname = "darkreader";
+    version = "4.9.23";
+    addonId = "addon@darkreader.org";
+    url =
+      "https://addons.mozilla.org/firefox/downloads/file/3667405/dark_reader-4.9.23-an+fx.xpi";
+    sha256 = "bb063bbc1c098a1629711bf1d4c7cd96851c7fce14c327dcef296807f25f2c32";
+    meta = with stdenv.lib; {
+      homepage = "https://darkreader.org/";
+      description =
+        "Dark mode for every website. Take care of your eyes, use dark theme for night and daily browsing.";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+  };
+}
--- modules/programs/browsers/firefox/default.nix
+++ modules/programs/browsers/firefox/default.nix
@@ -1,16 +1,20 @@
 { pkgs, lib, config, ... }:
-
-{
+with lib;
+let
+  buildFirefoxXpiAddon = pkgs.nur.rycee.firefox-addons.buildFirefoxXpiAddon;
+  addons = pkgs.callPackage ./addons.nix { inherit buildFirefoxXpiAddon; };
+in {
   config = lib.mkIf (config.dotfiles.browsers.firefox.enable) {
     programs.firefox = {
       enable = true;
       package = pkgs.latest.firefox-bin.override ({ pname = "firefox"; });
-      extensions = with pkgs.nur.rycee.firefox-addons; [
-        ublock-origin
-        lastpass-password-manager
-        reddit-enhancement-suite
-        facebook-container
-      ];
+      extensions = with pkgs.nur.rycee.firefox-addons;
+        [
+          ublock-origin
+          lastpass-password-manager
+          reddit-enhancement-suite
+          facebook-container
+        ] ++ (with addons; [ darkreader ]);
       profiles.default = {
         id = 0;
         settings = {
