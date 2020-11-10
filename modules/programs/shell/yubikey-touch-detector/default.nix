48d7f6269b2419c82d3f42bce0f358ff3ea7e957
Ian Johannesen <ij@opsplaza.com>
2020-11-10 22:43:01 +0100

    Fix watcher

--- modules/programs/shell/yubikey-touch-detector/default.nix
+++ modules/programs/shell/yubikey-touch-detector/default.nix
@@ -2,31 +2,36 @@
 with lib; {
   config = mkIf (config.dotfiles.shell.yubikey-touch-detector.enable) {
     home.packages = [ pkgs.yubikey-touch-detector ];
-    systemd.user = {
-      services.yubikey-touch-detector = {
-        Unit = {
-          Description = "Yubikey touch detector daemon";
+    systemd.user =
+      let
+        service-yubikey-touch-detector = pkgs.writeShellScriptBin "start-yubikey-touch-detector" (
+          ''
+            systemctl --user import-environment
+            export PATH="$PATH:$HOME/.nix-profile/bin"
+            ${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector
+          ''
+        );
+      in
+      {
+        services.yubikey-touch-detector = {
+          Unit = { Description = "Yubikey touch detector daemon"; After = [ "graphical.target" ]; };
+          Service = {
+            ExecStart = "${service-yubikey-touch-detector}/bin/start-yubikey-touch-detector";
+            Environment = [
+              "YUBIKEY_TOUCH_DETECTOR_VERBOSE=true"
+              "YUBIKEY_TOUCH_DETECTOR_LIBNOTIFY=true"
+            ];
+          };
+          Install = { WantedBy = [ "default.target" ]; };
         };
-        Install = {
-          WantedBy = [ "default.target" ];
-        };
-
-        Service = {
-          ExecStart = "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector";
-          Environment = [
-            "YUBIKEY_TOUCH_DETECTOR_VERBOSE=true"
-            "YUBIKEY_TOUCH_DETECTOR_LIBNOTIFY=true"
-          ];
+        sockets.yubikey-touch-detector = {
+          Unit = { Description = "Socket for yubikey touch detector daemon"; };
+          Socket = {
+            ListenStream = "%t/yubikey-touch-detector.socket";
+            RemoveOnStop = "yes";
+          };
+          Install = { WantedBy = [ "sockets.target" ]; };
         };
       };
-      sockets.yubikey-touch-detector = {
-        Unit = { Description = "Socket for yubikey touch detector daemon"; };
-        Socket = {
-          ListenStream = "%t/yubikey-touch-detector.socket";
-          RemoveOnStop = "yes";
-        };
-        Install = { WantedBy = [ "sockets.target" ]; };
-      };
-    };
   };
 }
