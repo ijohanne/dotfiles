25731f2268f183e803e5e092868bc420c467ddec
Ian Johannesen <ij@opsplaza.com>
2020-11-14 16:04:24 +0100

    Refactoring

--- modules/overlays/localpkgs/fish-plugins.nix
+++ modules/overlays/localpkgs/fish-plugins.nix
@@ -1,41 +1,42 @@
 { self, sources }:
-rec
+with self;
+with sources;
 {
   bass = {
     name = "bass";
-    src =
-      self.fetchFromGitHub { inherit (sources.bass) owner repo rev sha256; };
+    src = fetchFromGitHub {
+      inherit (bass) owner repo rev sha256;
+    };
   };
   oh-my-fish-plugin-ssh = {
     name = "oh-my-fish-plugin-ssh";
-    src = self.fetchFromGitHub {
-      inherit (sources.oh-my-fish-plugin-ssh) owner repo rev sha256;
+    src = fetchFromGitHub {
+      inherit (oh-my-fish-plugin-ssh) owner repo rev sha256;
     };
   };
   oh-my-fish-plugin-foreign-env = {
     name = "oh-my-fish-plugin-foreign-env";
-    src = self.fetchFromGitHub {
-      inherit (sources.oh-my-fish-plugin-foreign-env) owner repo rev sha256;
+    src = fetchFromGitHub {
+      inherit (oh-my-fish-plugin-foreign-env) owner repo rev sha256;
     };
   };
 
   fish-ssh-agent = {
     name = "fish-ssh-agent";
-    src = self.fetchFromGitHub {
-      inherit (sources.fish-ssh-agent) owner repo rev sha256;
+    src = fetchFromGitHub {
+      inherit (fish-ssh-agent) owner repo rev sha256;
     };
   };
   fzf-fish = {
     name = "fzf-fish";
-    src = self.fetchFromGitHub {
-      inherit (sources.fzf-fish) owner repo rev sha256;
+    src = fetchFromGitHub {
+      inherit (fzf-fish) owner repo rev sha256;
     };
   };
   fish-exa = {
     name = "fish-exa";
-    src = self.fetchFromGitHub {
-      inherit (sources.fish-exa) owner repo rev sha256;
+    src = fetchFromGitHub {
+      inherit (fish-exa) owner repo rev sha256;
     };
   };
-
 }
--- modules/overlays/localpkgs/vim-plugins.nix
+++ modules/overlays/localpkgs/vim-plugins.nix
@@ -1,45 +1,47 @@
 { self, sources }:
+with self.vimUtils;
+with sources;
 {
-  nvim-web-devicons = self.vimUtils.buildVimPluginFrom2Nix {
+  nvim-web-devicons = buildVimPluginFrom2Nix {
     name = "nvim-web-devicons";
     src = self.fetchFromGitHub {
-      inherit (sources.nvim-web-devicons) owner repo rev sha256;
+      inherit (nvim-web-devicons) owner repo rev sha256;
     };
   };
-  nvim-lsp-extensions = self.vimUtils.buildVimPluginFrom2Nix {
+  nvim-lsp-extensions = buildVimPluginFrom2Nix {
     name = "nvim-lsp-extensions";
     src = self.fetchFromGitHub {
-      inherit (sources.lsp_extensions-nvim) owner repo rev sha256;
+      inherit (lsp_extensions-nvim) owner repo rev sha256;
     };
   };
-  nvim-lspconfig = self.vimUtils.buildVimPluginFrom2Nix {
+  nvim-lspconfig = buildVimPluginFrom2Nix {
     name = "nvim-lspconfig";
     src = self.fetchFromGitHub {
-      inherit (sources.nvim-lspconfig) owner repo rev sha256;
+      inherit (nvim-lspconfig) owner repo rev sha256;
     };
   };
-  Language-tool-nvim = self.vimUtils.buildVimPluginFrom2Nix {
+  Language-tool-nvim = buildVimPluginFrom2Nix {
     name = "Language-tool-nvim";
     src = self.fetchFromGitHub {
-      inherit (sources.LanguageTool-nvim) owner repo rev sha256;
+      inherit (LanguageTool-nvim) owner repo rev sha256;
     };
   };
-  neovim-treesitter = self.vimUtils.buildVimPluginFrom2Nix {
+  neovim-treesitter = buildVimPluginFrom2Nix {
     name = "neovim-treesitter";
     src = self.fetchFromGitHub {
-      inherit (sources.nvim-treesitter) owner repo rev sha256;
+      inherit (nvim-treesitter) owner repo rev sha256;
     };
   };
-  ranger-vim = self.vimUtils.buildVimPluginFrom2Nix {
+  ranger-vim = buildVimPluginFrom2Nix {
     name = "ranger-vim";
     src = self.fetchFromGitHub {
-      inherit (sources.ranger-vim) owner repo rev sha256;
+      inherit (ranger-vim) owner repo rev sha256;
     };
   };
-  git-blame-nvim = self.vimUtils.buildVimPluginFrom2Nix {
+  git-blame-nvim = buildVimPluginFrom2Nix {
     name = "git-blame-nvim";
     src = self.fetchFromGitHub {
-      inherit (sources.git-blame-nvim) owner repo rev sha256;
+      inherit (git-blame-nvim) owner repo rev sha256;
     };
   };
 }
