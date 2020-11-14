6b2d4dc922351cedc97b0eae14f476ff422efff6
Ian Johannesen <ij@opsplaza.com>
2020-11-14 16:07:50 +0100

    Restructuring

--- modules/overlays/localpkgs/default.nix
+++ modules/overlays/localpkgs/default.nix
@@ -2,16 +2,16 @@
 self: super:
 {
   niv-sources = sources;
-  lib = (super.lib or { }) // import ./lib.nix { inherit self sources; };
-  bottom = self.callPackage ./bottom.nix { inherit sources; };
-  lfs = self.callPackage ./lfs.nix { inherit sources; };
-  pueue = self.callPackage ./pueue.nix { inherit sources; };
-  themes = self.libsForQt5.callPackage ./sddm-themes.nix { inherit sources; };
+  lib = (super.lib or { }) // import ./lib { inherit self sources; };
+  bottom = self.callPackage ./bottom { inherit sources; };
+  lfs = self.callPackage ./lfs { inherit sources; };
+  pueue = self.callPackage ./pueue { inherit sources; };
+  themes = self.libsForQt5.callPackage ./sddm-themes { inherit sources; };
   yubikey-touch-detector = self.callPackage ./yubikey-touch-detector { inherit sources; };
-  lua-language-server = self.callPackage ./lua-language-server.nix { inherit sources; };
-  gping = self.callPackage ./gping.nix { inherit sources; };
-  fishPlugins = (super.fishPlugins or { }) // import ./fish-plugins.nix { inherit self sources; };
-  vimPlugins = (super.vimPlugins or { }) // import ./vim-plugins.nix { inherit self sources; };
+  lua-language-server = self.callPackage ./lua-language-server { inherit sources; };
+  gping = self.callPackage ./gping { inherit sources; };
+  fishPlugins = (super.fishPlugins or { }) // import ./fish-plugins { inherit self sources; };
+  vimPlugins = (super.vimPlugins or { }) // import ./vim-plugins { inherit self sources; };
   niv = import sources.niv { };
   neovim-nightly = self.neovim-unwrapped.overrideAttrs (attrs: {
     pname = "neovim-nightly";
