23323eaa2bd12af04319973638ba3b36028f5045
Ian Johannesen <ij@opsplaza.com>
2020-11-25 11:50:21 +0100

    Update deps

--- modules/localpkgs/default.nix
+++ modules/localpkgs/default.nix
@@ -2,12 +2,4 @@
 _: pkgs: {
   niv-sources = sources;
   lib = (pkgs.lib or { }) // import ./lib { inherit pkgs sources; };
-  # This is currenctly broken in nixos-unstable-small, so ignore the tests for now
-  mercurial = pkgs.mercurial.overrideAttrs (_: {
-    propagatedBuildInputs = [
-      (pkgs.python38Packages.dulwich.overrideAttrs (_: {
-        doInstallCheck = false;
-      }))
-    ];
-  });
 }
