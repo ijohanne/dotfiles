9437fa8911159101a0edcc2610fbeeb1d8ad6d9c
Ian Johannesen <ij@opsplaza.com>
2021-09-10 03:07:04 +0200

    features(desktop): pin proper printer IP

--- modules/machines/desktop/printers.nix
+++ modules/machines/desktop/printers.nix
@@ -7,7 +7,7 @@ with lib; {
       ensurePrinters = [{
         description = "Home - HP OfficeJet 3830";
         name = "officejet_3830";
-        deviceUri = "ipp://10.255.100.2/ipp/print";
+        deviceUri = "ipp://10.255.100.230/ipp/print";
         ppdOptions = { PageSize = "A4"; };
         model = "drv:///hp/hpcups.drv/hp-officejet_3830_series.ppd";
       }];
