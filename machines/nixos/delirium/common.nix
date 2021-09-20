42d978e762449d67347b234b87a9cbf6b1629a39
Ian Johannesen <ij@opsplaza.com>
2021-09-20 17:44:19 +0000

    bugs(cachix): setup properly

--- machines/nixos/delirium/common.nix
+++ machines/nixos/delirium/common.nix
@@ -4,10 +4,12 @@
   environment.systemPackages = with pkgs; [ ripgrep vim nixpkgs-fmt htop fish git ethtool ];
   hardware.cpu.intel.updateMicrocode = true;
   nix.maxJobs = lib.mkDefault 64;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
     binaryCaches = [ "https://ijohanne.cachix.org" ];
     binaryCachePublicKeys = [ "ijohanne.cachix.org-1:oDy0m6h+CimPEcaUPaTZpEyVk6FVFpYPAXrrA9L5i9M=" ];
   };
--- machines/nixos/goose/common.nix
+++ machines/nixos/goose/common.nix
@@ -3,10 +3,12 @@
 {
   environment.systemPackages = with pkgs; [ ripgrep vim nixpkgs-fmt htop fish git ethtool ppp tcpdump conntrack-tools lm_sensors ];
   nix.maxJobs = lib.mkDefault 64;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
     binaryCaches = [ "https://ijohanne.cachix.org" ];
     binaryCachePublicKeys = [ "ijohanne.cachix.org-1:oDy0m6h+CimPEcaUPaTZpEyVk6FVFpYPAXrrA9L5i9M=" ];
   };
